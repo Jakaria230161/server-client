ed6ad6836537f65d5c40495bb512b01eae6a4732
Jakaria230161 <jakariamahmud016@gmail.com>
2022-11-01 17:42:52 +0600

    event handler added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect( () => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form onSubmit={handleAddUser}>
+        <input type='text' name='name'></input>
+        <br />
+        <input type='email' name='email'></input>
+        <br />
+        <button type='submit'>Add User</button>
+      </form>
+      <h1>User:{users.length}</h1>
+      <div>
+        {
+          users.map(user => <p key={user.id}> {user.name} {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
