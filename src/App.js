6370e22d680022cf483bea0a2b5de2b0d172a55b
Jakaria230161 <jakariamahmud016@gmail.com>
2022-11-01 18:22:50 +0600

    initial cmmit

--- src/App.js
+++ src/App.js
@@ -13,15 +13,31 @@ function App() {
 
   const handleAddUser = event => {
     event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+    
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+    event.target.reset();
   }
 
   return (
     <div className="App">
       
       <form onSubmit={handleAddUser}>
-        <input type='text' name='name'></input>
+        <input type='text' name='name' placeholder='enter your name' required></input>
         <br />
-        <input type='email' name='email'></input>
+        <input type='email' name='email' placeholder='enter your email' required></input>
         <br />
         <button type='submit'>Add User</button>
       </form>
